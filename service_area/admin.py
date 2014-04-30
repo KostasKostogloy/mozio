07cb4141c18687d3f5cb6ba26d4f96853384b5c0
Kostas <kostaskostogloy@outlook.com>
2014-04-30 21:52:58 +0300

    Basic model structure and django admin

---
+++ service_area/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from service_area.models import ServiceArea, Supplier, Vertice
+
+# Register your models here.
+admin.site.register(ServiceArea)
+admin.site.register(Supplier)
+admin.site.register(Vertice)
---
+++ service_area/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+
+class Supplier(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    def __str__(self):
+        return self.first_name + '.' +self.last_name
+
+class ServiceArea(models.Model):
+    supplier = models.ForeignKey(Supplier)
+    x1 = models.DecimalField( max_digits=14, decimal_places=12)
+    y1 = models.DecimalField( max_digits=15, decimal_places=12)
+    x2 = models.DecimalField( max_digits=14, decimal_places=12)
+    y2 = models.DecimalField( max_digits=15, decimal_places=12)
+    def __str__(self):
+        return self.id
+    
+class Vertice(models.Model):
+    ServiceArea = models.ForeignKey(ServiceArea)
+    longitude   = models.DecimalField( max_digits=15, decimal_places=12)
+    latitude    = models.DecimalField( max_digits=14, decimal_places=12)
+    def __str__(self):
+        return self.id
+     
+
